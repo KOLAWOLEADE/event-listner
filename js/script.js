99603d5b200aa423011da7ef11d980066addbde6
Kolawole Adeyemo <kolawole_adeyemo@yahoo.com>
2022-05-29 16:48:03 +0100

    add JQuery

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// jQuery(document).ready(function(){
+
+//     jQuery("h1").click(function() {
+
+//         alert("This is a heading.");
+    
+//       });
+    
+//       jQuery("p").click(function() {
+//         alert("This is a paragraph.");
+//       });
+    
+//       jQuery("img").click(function() {
+//           alert("This is an image");
+//       });
+
+// });
+
+// $  === JQuery
+
+
+$(document).ready(function(){
+
+    $("h1").click(function() {
+
+        alert("This is a heading.");
+    
+      });
+    
+      $("p").click(function() {
+        alert("This is a paragraph.");
+      });
+    
+      $("img").click(function() {
+          alert("This is an image");
+      });
+
+});
